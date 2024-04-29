395641755ca542bf677643fc1062d6a42a17fb3c
Yuxin Chen <che21007@byui.edu>
2024-04-29 09:59:38 -0600

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Net;
+using System.Runtime.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rdm = new Random();
+        int magic = rdm.Next(1, 101);
+
+        int guess;
+        int i = 0;
+        string again = "yes";
+        
+        while (again == "yes")
+        {
+            do
+            {
+                Console.WriteLine("What is the magic number?");
+                guess = int.Parse(Console.ReadLine());
+                if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                i += 1;
+            } while (guess != magic);
+
+            Console.WriteLine($"You took {i} time(s) to guess it.");
+            Console.WriteLine("Do you want to play again?");
+            again = Console.ReadLine();
+        }
+
+        
+
+
+
+
     }
 }
