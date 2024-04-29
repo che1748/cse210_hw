a72e017e748d1e4540af91e450726528192da6b6
Yuxin Chen <che21007@byui.edu>
2024-04-29 11:10:02 -0600

    completed prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        do
+        {
+            Console.WriteLine("Enter number (0 to quit): ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            } 
+        } while (number != 0);
+
+        int sum = 0;
+        int largest = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > largest)
+            {
+                largest = num;
+            }
+
+        }
+
+        float ave = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {ave}");
+        Console.WriteLine($"The largest number is {largest}");
     }
 }
