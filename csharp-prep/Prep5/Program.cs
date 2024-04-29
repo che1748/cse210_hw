72cd89676168e85509b7098c652603e7425a64a8
Yuxin Chen <che21007@byui.edu>
2024-04-29 11:59:14 -0600

    compelted prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = ProptUserName();
+        int num = PromptUserNumber();
+        int square = SquareNumber(num);
+        DisplayResult(username, square);
+
+
+
     }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+    
+    static string ProptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string username = Console.ReadLine();
+        return username;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int FavNum = int.Parse(Console.ReadLine());
+        return FavNum;
+    }
+    static int SquareNumber(int FavNum)
+    {
+        int square = FavNum * FavNum;
+        return square;
+    }
+
+    static void DisplayResult(string username, int square)
+    {
+        Console.WriteLine($"{username}, the square of your number is {square}");
+    }
+
 }
