96d9665cc1307c24787f3b9b878f28eae2fac712
Yuxin Chen <che21007@byui.edu>
2024-05-06 12:42:48 -0600

    Wirte code to use the classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._JobTitle = "software engineer";
+        job1._company = "Microsoft";
+        job1._StartYear = 2009;
+        job1._EndYear = 2023;
+        job1.DisplayInformation();
+
+        Job job2 = new Job();
+        job2._JobTitle = "manager";
+        job2._company = "Apple";
+        job2._StartYear = 2010;
+        job2._EndYear = 2020;
+        job2.DisplayInformation();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Andrew Chen";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
+
+
     }
 }
