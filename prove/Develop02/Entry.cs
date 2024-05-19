77b4c67ccafe164771aae55b2c3a71969e34f0ad
Yuxin Chen <che21007@byui.edu>
2024-05-18 20:00:25 -0600

    a class that hel record information

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -2,20 +2,23 @@ using System;
 
 public class Entry
 {
-    public List<string> _respondedPropmt;
-    public List<string> _response;
-    public List<string> _date;
+    public List<string> _respondedPropmt = new List<string>();
+    public List<string> _response = new List<string>();
+    public List<string> _date = new List<string>();
+    
 
     public void DisplayEntry()
     {
         
-        for (int i=0; i<_respondedPropmt.Count;i++)
+        for (int i=0; i<(_respondedPropmt.Count);i++)
         {
+            Console.WriteLine("--------------------------------");
             Console.WriteLine(_date[i]);
             Console.WriteLine(_respondedPropmt[i]);
             Console.WriteLine(_response[i]);
-
         }
+        Console.WriteLine("--------------------------------");
+
     }
 }
 
