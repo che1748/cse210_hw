13860ae3e03bef7ba5ca781cc7931d4c9fe61751
Yuxin Chen <che21007@byui.edu>
2024-05-28 10:26:02 -0600

    It doesn't hide the words that are already hidden

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference ref_ = new Reference("Ether", "12", "27");
+        string scriptureQuote = "And if men come unto me I will show unto them their weakness. I give unto men weakness that they may be humble; and my grace is sufficient for all men that humble themselves before me; for if they humble themselves before me, and have faith in me, then will I make weak things become strong unto them.";
+        Scripture scrip_ = new Scripture(ref_, scriptureQuote);
+
+        string userInput;
+        scrip_.Display();
+        do
+        {
+            Console.WriteLine("Press enter to continue or type 'quit' to finish");
+            userInput = Console.ReadLine();
+
+            Console.Clear();
+            scrip_.HideWords();
+            scrip_.Display();
+
+            if (scrip_.IsItCompletelyHidden()) 
+            {
+                Console.WriteLine("\nCongratulations. You now have memorized the scripture!");
+                break;
+            }
+
+        } while (userInput != "quit");
+
+        Console.WriteLine("Thank you for using the program!");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        List<string> test = new List<string>()
+        {
+            "apple","bee","cat"
+        };
+
+        Console.Write(test[0]);
+        Console.WriteLine(test.Count);
+
+        for (int i=0; i<4; i++)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
