c609b2c0c3504cf35e6d05c1f693d118d88791fd
Yuxin Chen <che21007@byui.edu>
2024-05-28 21:01:20 -0600

    added a second constructor

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -5,16 +5,26 @@ public class Reference
     private string _book;
     private string _chapter;
     private string _verse;
+    private string _endverse;
 
     public Reference(string book, string chapter, string verse)
     {
         _book = book;
         _chapter = chapter;
         _verse = verse;
+        _endverse = "";
+    }
+
+    public Reference(string book, string chapter, string verse, string endverse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endverse = $"-{endverse}";     
     }
 
     public string ReferenceInString()
     {
-        return $"{_book}, {_chapter}:{_verse}";
+        return $"{_book}, {_chapter}:{_verse}{_endverse}";
     }
 }
