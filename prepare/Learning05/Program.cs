f35b6c2664547cc25b1bf101ba8e0bc667d4c4f5
Yuxin Chen <che21007@byui.edu>
2024-06-18 13:39:25 -0600

    finished learning activity5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square shape1 = new Square("blue", 3);
+        Rectangle shape2 = new Rectangle("Yellow", 2, 4);
+        Circle shape3 = new Circle("green", 4);
+
+        List<Shape> shapeList = new List<Shape>()
+        {
+            shape1, shape2, shape3
+        };
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
